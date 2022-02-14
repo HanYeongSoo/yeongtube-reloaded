5fa431974fad1f3ad6f0e7e1a6860122beba282e
Yeong Soo <besthys7@gmail.com>
2022-02-14 20:35:41 +0900

    3. Get request and response

--- src/server.js
+++ src/server.js
@@ -7,6 +7,16 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("This is home");
+};
+const handleLogin = (req, res) => {
+  return res.send("This is Login Page!");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🛺`);
 
