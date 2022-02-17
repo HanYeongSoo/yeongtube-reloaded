9545916ee2e8ef3b0e703fb643ff1dd9ff4964c7
Yeong Soo <besthys7@gmail.com>
2022-02-17 19:05:15 +0900

    5. External Middlewares (morgan)

--- src/server.js
+++ src/server.js
@@ -2,36 +2,24 @@
 // const express = require("express");  위에랑 같은 문장인데 윗 문장이 더 직설적으로 보임
 
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
-const app = express();
+const app = express(); // 이 한줄이 express application을 사용할 수 있게 해줌
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed!!");
-  }
-  console.log("Allowed, you continue.");
-  next();
-};
+// const logger = (req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// };
 
 const handleHome = (req, res) => {
   return res.send("This is home");
 };
-const handleLogin = (req, res) => {
-  return res.send("This is Login Page!");
-};
 
 app.use(logger);
-app.get("/", handleHome);
-app.get("/protected", privateMiddleware);
-app.get("/login", handleLogin);
+app.get("/", handleHome); // route를 만들고 그 안에 handler를 만드는거지
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🛺`);
