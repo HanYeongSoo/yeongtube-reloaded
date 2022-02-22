0e9af5285cfd19b08004d1e80004e0d22eec93af
Yeong Soo <besthys7@gmail.com>
2022-02-22 20:05:48 +0900

    11. URL Parameters (+ 정규식)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,15 @@
 export const trending = (req, res) => res.send("HomePage Videos입니다.");
-export const see = (req, res) => res.send("watch Videos입니다.");
-export const edit = (req, res) => res.send("edit Videos입니다.");
+export const see = (req, res) => {
+  console.log(req.params);
+  return res.send(`Watch Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  console.log(req.params);
+  return res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send("Delete Video");
+};
