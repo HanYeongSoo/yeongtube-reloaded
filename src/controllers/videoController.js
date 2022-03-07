8cca72f43b0e898bcfbe7a932523ab4573950ae8
Yeong Soo <besthys7@gmail.com>
2022-03-07 22:17:07 +0900

    21. Array Database _01

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,35 +1,38 @@
+let videos = [
+  {
+    title: "소년심판",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "DP",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "호텔델루나",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "소년심판",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "DP",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "호텔델루나",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => {
   console.log(req.params);
-  return res.render("watch");
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 export const edit = (req, res) => {
   console.log(req.params);
